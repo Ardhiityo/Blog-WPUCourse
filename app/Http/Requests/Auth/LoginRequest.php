cad80d4023e74839475aed83469f1cef1f389a23
belajar-vue-2 <aryaadi229@gmail.com>
2025-07-11 15:49:20 +0700

    Implement authentication routes, console commands, and profile management features
    - Added authentication routes including registration, login, password reset, and email verification in `routes/auth.php`.
    - Created console command for displaying inspiring quotes in `routes/console.php`.
    - Defined web routes for dashboard and profile management in `routes/web.php`.
    - Updated `.gitignore` files in various storage directories to exclude unnecessary files.
    - Configured Tailwind CSS in `tailwind.config.js` for styling.
    - Developed feature tests for authentication, email verification, password confirmation, password reset, and profile management in the `tests/Feature/Auth` directory.
    - Created base test case structure in `tests/TestCase.php` and example tests in `tests/Unit/ExampleTest.php`.
    - Set up Vite configuration for asset management in `vite.config.js`.

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+
+class LoginRequest extends FormRequest
+{
+    protected $auth_credential;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->auth_credential = request()->input('credential');
+
+        $this->auth_credential = filter_var($this->auth_credential, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        $this->merge([
+            $this->auth_credential => $this->credential
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'credential' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function authenticate(): void
+    {
+        $this->ensureIsNotRateLimited();
+
+        if (! Auth::attempt($this->only($this->auth_credential, 'password'), $this->boolean('remember'))) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'credential' => trans('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     */
+    public function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->string('email')) . '|' . $this->ip());
+    }
+}
